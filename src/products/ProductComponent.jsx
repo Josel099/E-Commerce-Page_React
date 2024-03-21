1f291ead291bd6d73681cc416ae528e9ded0cfc6
DESKTOP-2STDA9J\Livares <josel.x@livares.com>
2024-03-21 11:55:04 +0530

    select box placed in product component.

--- src/products/ProductComponent.jsx
+++ src/products/ProductComponent.jsx
@@ -18,6 +18,15 @@ return (
    <h3>{product.title}</h3>
    <h4>{product.price} ₹</h4>
    <p className='description' >{product.description}</p>
+
+   <select name="" id="">
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="4">5</option>
+    </select>
+    
    <button onClick={()=>{props.addtoCart(product)}} >Add to cart</button>
  </div>
  
